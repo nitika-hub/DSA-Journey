d5fe1f08427a58b207ddf2ceda57bd371e8c4d47
nitika-hub <nitika000n@gmail.com>
2026-09-15 23:54:21 +0530

    Added solution to print all the divisors of a number in an optimized way.

---
+++ Basic Maths/AllDivisorsOfNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AllDivisorsOfNumber {
+    public ArrayList<Integer> getDivisors(int n) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1; i*i<=n; i++){
+            if(n%i==0){
+                list.add(i);
+                if(i!=n/i){
+                    list.add(n/i);
+                }
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
